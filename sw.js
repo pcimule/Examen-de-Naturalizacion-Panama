78f044adcd631b4406dff3537e67f7e4cabcf813
Piero Cimule <68132264+pcimule@users.noreply.github.com>
2026-04-12 17:19:06 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'panama-examen-v2';
+const ARCHIVOS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+});
